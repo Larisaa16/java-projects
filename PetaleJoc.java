e487864aa015f03fbfa62f25645ea55f914459b5
Larisaa16 <107370531+Larisaa16@users.noreply.github.com>
2024-07-15 23:01:40 +0300

    Add files via upload

---
+++ PetaleJoc.java
@@ -0,0 +1,19 @@
+//cat de mult te iubeste cineva pe baza unui numar de petale introdus de utilizator
+
+import java.util.Scanner;
+
+public class PetaleJoc {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Dati nr de petale: ");
+        int x = scanner.nextInt();
+
+        String[] raspunsuri = {"deloc", "putin", "mult", "cu pasiune", "la nebunie", "foarte mult"};
+        String rsp = raspunsuri[x % 6];
+
+        System.out.println("el ma iubeste " + rsp);
+
+        scanner.close(); 
+   }
+}
