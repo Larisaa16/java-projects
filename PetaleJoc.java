cb691fdf3ccfe6bbeb1853aaf8cbdd1fb8547f30
Larisaa16 <ciuciularisa@gmail.com>
2024-08-02 18:07:53 +0300

    Rezolvare conflicte

--- PetaleJoc.java
+++ PetaleJoc.java
@@ -1,23 +1,4 @@
-<<<<<<< HEAD
-//cat de mult te iubeste cineva pe baza unui numar de petale introdus de utilizator
 
-import java.util.Scanner;
-
-public class PetaleJoc {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Dati nr de petale: ");
-        int x = scanner.nextInt();
-
-        String[] raspunsuri = {"deloc", "putin", "mult", "cu pasiune", "la nebunie", "foarte mult"};
-        String rsp = raspunsuri[x % 6];
-
-        System.out.println("el ma iubeste " + rsp);
-
-        scanner.close();
-   }
-=======
 //cat de mult te iubeste cineva pe baza unui numar de petale introdus de utilizator
 
 import java.util.Scanner;
@@ -36,5 +17,4 @@ public class PetaleJoc {
 
         scanner.close(); 
    }
->>>>>>> de0d29561db95237957aa63e13e4d3887d9253f6
 }
