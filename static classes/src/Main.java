cc91cc8320c3f3300a74ba714242dcc452cde602
Larisaa16 <ciuciularisa@gmail.com>
2024-09-04 20:46:14 +0300

    First commit

---
+++ static classes/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        Friend friend1= new Friend("Val");
+        Friend friend2= new Friend("Sylvia");
+
+        //System.out.println(Friend.numberOffFriends);
+
+        Friend.displayFriends();
+    }
+}
